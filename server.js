8a5dd8e5a5b735b5f0fea9183afefd8914fb891d
Jacquelineomollo <Jacquelineomollo@gmail.com>
2020-10-15 13:42:38 -0400

    setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+const {v4: uuidV4} = require("uuid")
+
+//setup for how to render the video
+app.set("view rngine" , "ejs" )
+// static folder that stores css, javacript and etc
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+  })
+io.on("connection", socket => {
+    socket.on("join-room", (roomId, userId) => {
+       socket.join(roomId)
+       socket.to().emit("user-connected", userId)
+
+       socket.on("disconnect", () => {
+           socket.to(roomId).broadcast.emit("user-disconnected", userId)
+       })
+    })
+})
+server.listen(8000)
